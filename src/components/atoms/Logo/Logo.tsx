4839b9116606d2dba8cdb4c1da41e13c6361d740
Andrei Ciceu <ciceu.andrei@gmail.com>
2021-01-18 00:42:50 +0200

    cleanup assets

--- src/components/atoms/Logo/Logo.tsx
+++ src/components/atoms/Logo/Logo.tsx
@@ -1,7 +1,9 @@
+import {logo} from '@assets/images'
+
 export default function Logo() {
   return (
     <div className="flex items-center">
-      <img key="logo" className="cursor-pointer h-12 m-2" src="/images/brand/logo.png" alt="Logo" />
+      <img key="logo" className="cursor-pointer h-12 m-2" src={logo} alt="Logo" />
       <img
         key="text"
         className="cursor-pointer h-12 m-2"
